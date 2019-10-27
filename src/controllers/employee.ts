6defc1bcc05b053531dcfa99ecc962897b8b1942
Felipe Gonzalez <fgonzalez@miningtag.cl>
2019-10-27 15:50:09 -0400

    employee first route complete, 'SELECT *'

--- src/controllers/employee.ts
+++ src/controllers/employee.ts
@@ -1,8 +1,26 @@
-import { Request, Response } from 'express';
+/**
+ * @author Felipe Gonzalez
+ * @description Service from entity employee
+ * @createdAt 25-10-2019
+ * @updateBy
+ * @descriptionUpdate
+ * @updateAt
+ */
 
-export class EmployeeService {
+import { connect } from '../database';
 
+export class EmployeeController {
+    /**
+     * Returns all employees from db
+     */
     public static getEmployees(){
-        
+        return new Promise((resolve, reject) => {
+            const con = connect();
+            con.query('SELECT * FROM employee')
+            .then(
+                (result) => resolve(result[0]),
+                (error) => reject(error)
+            );
+        });
     }
 }
--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -1,9 +1,26 @@
 import { Request, Response, Router } from 'express';
+import { EmployeeController } from '../controllers/employee';
+import { IResponse } from '../interface/response';
 const router: Router = Router();
 
 
 router.get("/", (req: Request, res: Response) =>{
-    res.json("Employee's");
+    EmployeeController.getEmployees().then(
+        (result) => {
+            const response: IResponse = {
+                data: result,
+                status: true
+            }
+            res.send(response);
+        },
+        (error) => {
+            const response: IResponse = {
+                status: false,
+                error
+            }
+            res.send(response);
+        }
+    );
 });
 
 export const employeeRoute: Router = router;
