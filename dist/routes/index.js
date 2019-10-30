d3581ee0a724d6981287ac5bcd10636c5203f5b0
Felipe Gonzalez <fgonzalez@miningtag.cl>
2019-10-30 19:27:17 -0400

    removed extra files

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,5 +4,4 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./employee"));
-__export(require("./indexPrueba"));
 //# sourceMappingURL=index.js.map
--- src/routes/indexPrueba.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { indexWelcome } from '../controllers';
-
-const router = Router();
-
-router.route('/').get(indexWelcome);
-
-export const IndexRoutes: Router = router;
