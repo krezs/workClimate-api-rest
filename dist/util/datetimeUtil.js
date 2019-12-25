8f75f367f0d11a6d5f9bd5e32710c042884af0de
Felipe Gonzalez <fgonzalez@miningtag.cl>
2019-12-25 15:54:39 -0400

    feature/login #comment added datetime util and install moment and moment-timezone

---
+++ dist/util/datetimeUtil.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * @author Felipe Gonzalez
+ * @description Class which has format method of datetime value to entities class
+ * @createdAt 23-11-2019
+ * @updateBy
+ * @descriptionUpdate
+ * @updateAt
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+class datetimeUtil {
+}
+exports.datetimeUtil = datetimeUtil;
+//# sourceMappingURL=datetimeUtil.js.map
---
+++ src/util/datetimeUtil.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Felipe Gonzalez
+ * @description Class which has format method of datetime value to entities class
+ * @createdAt 23-11-2019
+ * @updateBy
+ * @descriptionUpdate
+ * @updateAt
+ */
+
+ import moment from "moment-timezone";
+
+ export class datetimeUtil {
+
+ }
